7968b2f502c6871b37e23aceef50bce3eca88b2a
emsbn <emsbn1@gmail.com>
2021-01-06 18:11:59 +0900

    fastify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
